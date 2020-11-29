b37b3bbdf36e05945474a04bcd494fea74c12f4e
ma22npk <gunjo64@icloud.com>
2020-11-29 20:55:33 +0900

    SimpleDateFormatで任意のフォーマットを作成して出力するプログラム

---
+++ src/about_simpleDateFormat/MainSimpleDateFormat.java
@@ -0,0 +1,20 @@
+package about_simpleDateFormat;
+
+// ① ここで SimpleDateFormat と Dateをインポート
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainSimpleDateFormat {
+
+    public static void main(String[] args) {
+
+        // ② 現在時刻を取得してくる値を 変数 date に格納
+        Date date = new Date();
+        // SimpleDateFormat をオブジェクト化し、任意のフォーマットを設定
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年 MM月 dd日");
+        // フォーマット指定なし
+        System.out.println(date);
+        // フォーマット指定あり
+        System.out.println(sdf.format(date));
+    }
+}
